5341fb75dbf2c80a336569f8cae6e181afe00df7
Cyclops200 <75571829+Cyclops200@users.noreply.github.com>
2021-01-26 20:37:30 +0700

    latihan03.py

--- Function/latihan03.py
+++ Function/latihan03.py
@@ -1,11 +1,17 @@
 # latihan projek python 3
 
-def bintang(n):
+def bintangA(n):
     for i in range(1, n+1):
         for j in range(i):
             print('*', end='')
         print()
+
+def bintangB(n):
     for i in range(n):
         for j in range(n-i):
             print('*', end='')
         print()
+
+def bintangC(n):
+    bintangA(n//2)
+    bintangB(n-n//2)
