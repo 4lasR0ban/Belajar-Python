a51ce6d58ce3ee2ecfbb3201e831d9d3e0049cce
Cyclops200 <75571829+Cyclops200@users.noreply.github.com>
2021-02-02 19:49:22 +0700

    Function

--- Function/latihan03.py
+++ Function/latihan03.py
@@ -1,17 +1,20 @@
 # latihan projek python 3
 
+# star formation 1
 def bintangA(n):
     for i in range(1, n+1):
         for j in range(i):
             print('*', end='')
         print()
 
+# star formation 2
 def bintangB(n):
     for i in range(n):
         for j in range(n-i):
             print('*', end='')
         print()
 
+# star formation gabungan
 def bintangC(n):
     bintangA(n//2)
     bintangB(n-n//2)
